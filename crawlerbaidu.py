bad83e0b05f363e9e905e2a7be6c7398ec7e3e6a
linhanqiu <linhanqiu1123@163.com>
2018-01-23 03:04:09 +0800

    新增百度地图爬虫

---
+++ crawlerbaidu.py
@@ -0,0 +1,16 @@
+import abc
+# class A(metaclass=abc.ABCMeta):
+
+# A.register(list)
+# print(isinstance([],A))
+# class A:
+#     def __init__(self):
+#         pass
+#
+#     def a(self):
+#         return
+#     c = "a"
+#     def call(self):
+#         return callable(self.c)
+# a = A()
+# print(a.call())
